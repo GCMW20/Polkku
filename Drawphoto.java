5e520a7ecd98bb12a0eb05c5a1e27bae3b1b775d
yerin1106 <51941176+yerin1106@users.noreply.github.com>
2020-05-19 15:40:06 +0900

    Create Drawphoto.java

---
+++ Drawphoto.java
@@ -0,0 +1,16 @@
+package gachon.mp2020.polkku2;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Drawphoto extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.drawphoto);
+    }
+}
