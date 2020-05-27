0d8edbd1e29c9609601f1cdf22e65f9b3b57d06f
MyungjiSong <63574436+MyungjiSong@users.noreply.github.com>
2020-05-27 12:10:06 +0900

    Update and rename Drawphoto.java to Editphoto.java

--- Editphoto.java
+++ Editphoto.java
@@ -11,6 +11,12 @@ public class Drawphoto extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.drawphoto);
+        setContentView(R.layout.editphoto);
+
+        ImageView imageView = (ImageView)findViewById(R.id.imageView);
+
+        byte[] byteArray = getIntent().getByteArrayExtra("image");
+        Bitmap bitmap = BitmapFactory.decodeByteArray(byteArray,0,byteArray.length);
+        imageView.setImageBitmap(bitmap);
     }
 }
